f5c32b9826fb9b4c9a26d69369c8a6acb4023258
AHMED NABIB <nabibpallab@gmail.com>
2022-09-09 21:35:12 +0600

    initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [guns, setGuns] = useState([]);
+  console.log(guns);
+
+	useEffect(() => {
+		fetch("data.json")
+			.then((res) => res.json())
+			.then((data) => setGuns(data));
+	}, []);
+	return (
+		<div className="App">
+			<Navbar></Navbar>
+			<Card></Card>
+		</div>
+	);
 }
 
 export default App;
